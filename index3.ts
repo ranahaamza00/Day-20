37613d437af444cae0a913a0e3ea8b75eba904ef
ranahaamza00 <ranahaamza00@gmail.com>
2024-05-24 12:58:10 +0500

    first commit

---
+++ index3.ts
@@ -0,0 +1,15 @@
+
+let userProfile = (function() {
+    
+    let name = "John";
+    let age = 30;
+
+    
+    return {
+        displayInfo: function() {
+            console.log(`Name: ${name}, Age: ${age}`);
+        }
+    };
+})();
+
+userProfile.displayInfo(); 
